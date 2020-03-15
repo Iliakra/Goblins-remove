2e77e1deb5a06b6e45d09cef5ab888fc0899d656
Ilya Krasilnikov <kravus27@gmail.com>
2020-03-15 17:54:21 +0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const newImg = document.createElement('img');
+newImg.classList.add('goblin-image');
+newImg.setAttribute('src', './image/goblin.png');
+
+const positions = document.getElementsByClassName('field');
+let images = document.getElementsByClassName('goblin-image');
+const goblinImage = images[0];
+const positionsArray = [...positions];
+function getRandomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function removeGoblin() {
+  const i = getRandomInRange(0, 16);
+  const parentElement = positionsArray[i];
+  if (!parentElement.contains(goblinImage)) {
+    parentElement.appendChild(newImg);
+    images = [...images];
+  } else if (images.length > 1) {
+    images[0].remove();
+  } else {
+    removeGoblin();
+  }
+}
+
+
+setInterval(removeGoblin, 1000);
